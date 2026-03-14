930b96094c82762fe406d118e9fe2374550c431c
KishoreKumar0603 <kishorekumars5643@gmail.com>
2026-03-15 00:58:58 +0530

    before prasanth

--- ArmstrongNumber/src/App.jsx
+++ ArmstrongNumber/src/App.jsx
@@ -13,24 +13,25 @@ function App() {
       setText("");
       setDisplay("");
     };
-  return (
-   <>
-    <h1>ArmStrongNumber</h1>
-    <div className='Container'>
-      <label>Enter the Value</label>
-      <input type="text" value={text} onChange={
-        (e)=> setText(e.target.value)}
-      />
-      <br/>
-      <br/>
-      <button onClick={handleadd}>Clear</button>
-      <button onClick={handleClear}>Submit</button>
-    </div>
-    <div className='result'>
-      {display}
-    </div>
-   </>
-  )
+
+    return (
+    <>
+      <div className="h-screen w-screen h-screen w-screen flex justify-center items-center">
+        <div className='Container bg-gray-200 h-150 w-150'>
+          <h1>Armstrong Number Finder</h1>
+          <label>Enter the Value</label>
+          <input type="text" value={text} onChange={
+            (e)=> setText(e.target.value)}
+          />
+          <button onClick={handleadd}>Clear</button>
+          <button onClick={handleClear}>Submit</button>
+        </div>
+        <div className='result'>
+          {display}
+        </div>
+      </div>
+    </>
+    )
 }
 
 export default App
